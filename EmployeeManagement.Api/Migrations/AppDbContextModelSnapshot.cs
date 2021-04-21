52a2503ae45b16fe4498eb8a7275999bd0864486
inspire-coding <+PaZ+PeTi+88113+>
2021-04-21 13:17:30 +0200

    Authorization and Authentication

--- EmployeeManagement.Api/Migrations/AppDbContextModelSnapshot.cs
+++ EmployeeManagement.Api/Migrations/AppDbContextModelSnapshot.cs
@@ -73,18 +73,22 @@ namespace EmployeeManagement.Api.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("FirstName")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<int>("Gender")
                         .HasColumnType("int");
 
                     b.Property<string>("LastName")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("PhotoPath")
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("EmployeeId");
+
+                    b.HasIndex("DepartmentId");
 
                     b.ToTable("Employees");
 
@@ -133,6 +137,17 @@ namespace EmployeeManagement.Api.Migrations
                             LastName = "Longway",
                             PhotoPath = "images/sara.png"
                         });
+                });
+
+            modelBuilder.Entity("EmployeeManagement.Models.Employee", b =>
+                {
+                    b.HasOne("EmployeeManagement.Models.Department", "Department")
+                        .WithMany()
+                        .HasForeignKey("DepartmentId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Department");
                 });
 #pragma warning restore 612, 618
         }
